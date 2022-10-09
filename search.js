797947e5170b7f26e832fe36f34f2c47f43997af
amrix <76793908+aamrixx@users.noreply.github.com>
2022-10-09 17:27:59 +0100

    Delete search.js

--- search.js
+++
@@ -1,6 +0,0 @@
-const base_url = "https://google.com/search?q=";
-
-function search() {
-	var query = document.getElementById("searchbox").value;
-	window.location.replace(base_url + query.replace(/ /g, "+"));	
-}
