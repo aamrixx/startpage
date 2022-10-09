da30ab6f69403333eee66ecf80aba03de3ef2f8d
aamrixx <amrix@hotmail.co.uk>
2022-10-09 17:08:19 +0100

    Fix

--- search.js
+++ search.js
@@ -1,13 +1,5 @@
 const base_url = "https://google.com/search?q=";
 
-alert(document.getElementById("searchbox"));
-/*inputText.addEventListener("keyup", function(event) {
-	if (event.keyCode === 13) {
-		event.preventDefault();
-    	document.getElementById("searchbtn").click();
-    }
-});*/
-
 function search() {
 	var query = document.getElementById("searchbox").value;
 	window.location.replace(base_url + query.replace(/ /g, "+"));	
